6725caa80ddb6cdf3bac061e7b2dc5e4d9dcbc55
Aamir <aamirmaj@gmail.com>
2018-01-26 08:50:20 -0500

    web login

---
+++ web_login/facebook.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from getpass import getpass
+
+usr = input('Enter username/email: ')
+pwd = getpass('Enter password: ')
+
+driver = webdriver.Firefox()
+driver.get('https://www.facebook.com/')
+
+username_box = driver.find_element_by_id('email')
+username_box.send_keys(usr)
+password_box = driver.find_element_by_id('pass')
+password_box.send_keys(pwd)
+
+login_btn = driver.find_element_by_id('loginbutton')
+login_btn.submit()
